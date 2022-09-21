5eec96e6acca4dbfb31575c110e201e45e246e99
Adedunmola Oyewale <oyewaleadedunmola@gmail.com>
2022-09-21 13:31:29 +0100

    Create login
    create login endpoint, ensure users are able to login with their email
    and password, add verifyJWT middleware to protect routes and add tests
    for the login endpoint.
    
    [Deliver #login]

--- src/api/routes/auth.js
+++ src/api/routes/auth.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const {
   registerController,
+  loginController,
   forgotPasswordController,
   resetPasswordController,
 } = require('../controllers/auth');
@@ -8,6 +9,7 @@ const {
 const router = express.Router();
 
 router.route('/register').post(registerController);
+router.route('/login').post(loginController);
 router.route('/forgot-password').post(forgotPasswordController);
 router.route('/reset-password/:email/:token').patch(resetPasswordController);
 
--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const dotEnv = require('dotenv');
 require('express-async-errors');
+const { StatusCodes } = require('http-status-codes');
 const errorHandlerMiddleWare = require('./api/middlewares/errorHandler');
 const notFound = require('./api/middlewares/notFound');
-
+const verifyJWT = require('./api/middlewares/verifyJWT');
 const authRouter = require('./api/routes/auth');
 
 // Application instance should be below the above require statements
@@ -13,6 +14,13 @@ app.use(express.json());
 
 // Application routes are going to be here
 app.use('/api/v1/auth', authRouter);
+
+// Protected routes go below this middleware
+app.use(verifyJWT);
+
+app.use('/', async (req, res) =>
+  res.status(StatusCodes.OK).json({ message: 'Homepage' })
+);
 
 app.use(notFound);
 app.use(errorHandlerMiddleWare);
