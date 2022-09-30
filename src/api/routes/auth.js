fc5782e9a34510a21a31a6412bae24bbbfb7c10e
Adedunmola Oyewale <oyewaleadedunmola@gmail.com>
2022-09-30 10:49:09 +0100

    Create vendors registration
    create vendors registration endpoint, ensure vendors are able to register
    and add tests for the endpoint.
    
    [Deliver #vendors-registration]

--- src/api/routes/auth.js
+++ src/api/routes/auth.js
@@ -5,10 +5,12 @@ const {
   forgotPasswordController,
   resetPasswordController,
 } = require('../controllers/auth');
+const { vendorsReg } = require('../middlewares/utils');
 
 const router = express.Router();
 
-router.route('/register').post(registerController);
+router.route('/users/register').post(registerController);
+router.route('/vendors/register').post(vendorsReg, registerController);
 router.route('/login').post(loginController);
 router.route('/forgot-password').post(forgotPasswordController);
 router.route('/reset-password/:email/:token').patch(resetPasswordController);
